54c8636d5d0b6355e9ea8f0a3eb69fd73cbef601
Bu Kinoshita <bukinoshita@gmail.com>
2017-06-06 18:17:08 -0400

    Add XO & Throw erro

--- index.js
+++ index.js
@@ -4,11 +4,15 @@
 const hash = require('string-hash')
 const color = require('tinycolor2')
 
-module.exports = (uid) => {
-  const n = hash(uid)
-  const c1 = color({ h: n % 360, s: 0.95, l: 0.5 })
-  const c1_ = c1.toHexString()
-  const c2 = c1.triad()[1].toHexString()
+module.exports = uid => {
+  if (uid) {
+    const n = hash(uid)
+    const c1 = color({h: n % 360, s: 0.95, l: 0.5})
+    const c1_ = c1.toHexString()
+    const c2 = c1.triad()[1].toHexString()
 
-  return `linear-gradient(45deg, ${c1_} 10%, ${c2} 100%)`
+    return `linear-gradient(45deg, ${c1_} 10%, ${c2} 100%)`
+  }
+
+  throw new Error('uid is required')
 }
