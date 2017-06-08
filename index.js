cc532939e76e140ebb50aa5cec4bdb8b65e867ff
Bu Kinoshita <bukinoshita@gmail.com>
2017-06-07 21:06:47 -0400

    Add radial gradient type (https://github.com/bukinoshita/random-gradient/issues/1)

--- index.js
+++ index.js
@@ -4,14 +4,22 @@
 const hash = require('string-hash')
 const color = require('tinycolor2')
 
-module.exports = uid => {
+module.exports = (uid, type = 'diagonal') => {
   if (uid) {
     const n = hash(uid)
     const c1 = color({h: n % 360, s: 0.95, l: 0.5})
     const c1_ = c1.toHexString()
     const c2 = c1.triad()[1].toHexString()
 
-    return `linear-gradient(45deg, ${c1_} 10%, ${c2} 100%)`
+    switch (type) {
+      case 'diagonal':
+        return `linear-gradient(45deg, ${c1_} 10%, ${c2} 100%)`
+        break
+
+      case 'radial':
+        return `radial-gradient(${c1_}, ${c2})`
+        break
+    }
   }
 
   throw new TypeError('uid is required')
